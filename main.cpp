90961cf214df260cefa1e493a99fdac12195be7d
Stuka55 <63323662+Stuka55@users.noreply.github.com>
2020-08-19 20:55:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,3 +18,29 @@ int main()
     hello2Function(name2);
     return 0;
 }
+
+hello1Function
+/*
+* hello1Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+cout << "hello World " << name1 << endl;
+return 0;
+}
+hello1Function
+Lab 2
+/*
+* hello2Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello2Function(string name2)
+{
+cout << "hello World " << name2 << endl;
+return 0;
+}
